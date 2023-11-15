2c8282286c42f3f7d90970e708e9eb1a9e35b32d
kathy2470 <mukisakathy@gmail.com>
2023-11-15 15:30:09 +0300

    builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,10 +1,11 @@
-#include "shell.h"
+#include "my_shell.h"
 
 /**
  * _myhistory - displays the history list, one command by line, preceded
- * with line numbers, starting at 0.
+ *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
+ *
  * Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -14,11 +15,11 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string.
- * @info: parameter struct.
- * @str: the string alias.
+ * unset_alias - unsets an alias by removing it from the list.
+ * @info: parameter struct
+ * @str: the string alias
  *
- * Return: Always 0 on success, 1 on error.
+ * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
 {
@@ -37,11 +38,11 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * set_alias - sets alias to string.
- * @info: parameter struct.
- * @str: the string alias.
+ * set_alias - sets an alias to a string.
+ * @info: parameter struct
+ * @str: the string alias
  *
- * Return: Always 0 on success, 1 on error.
+ * Return: Always 0 on success, 1 on error
  */
 int set_alias(info_t *info, char *str)
 {
@@ -59,9 +60,9 @@ int set_alias(info_t *info, char *str)
 
 /**
  * print_alias - prints an alias string.
- * @node: the alias node.
+ * @node: the alias node
  *
- * Return: Always 0 on success, 1 on error.
+ * Return: Always 0 on success, 1 on error
  */
 int print_alias(list_t *node)
 {
@@ -84,7 +85,8 @@ int print_alias(list_t *node)
  * _myalias - alias builtin (man alias).
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * Return: Always 0.
+ *
+ * Return: Always 0
  */
 int _myalias(info_t *info)
 {
