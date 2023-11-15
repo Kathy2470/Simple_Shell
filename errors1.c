576a4a63d831ee65b7a798b389f55806d281e3f6
kathy2470 <mukisakathy@gmail.com>
2023-11-16 01:02:16 +0300

    update Team work

--- errors1.c
+++ errors1.c
@@ -1,4 +1,4 @@
-#include "my_shell.h"
+#include "shell.h"
 
 /**
  * _erratoi - converts a string to an integer.
--- exist.c
+++ exist.c
@@ -1,4 +1,4 @@
-#include "my_shell.h"
+#include "shell.h"
 
 /**
  * _strncpy - copies a string.
